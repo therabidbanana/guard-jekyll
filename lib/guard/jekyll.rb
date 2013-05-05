4a9f238131935722fdcde4736fe9c4a4d2dcea2b
Casper Von B <casperbeyer@gmail.com>
2013-05-05 12:26:19 +0800

    Rewrote the entire Guard::Jekyll class.
    Major rewrite of the Guard::Jekyll class.
    
    - Use options hash instead of class variables.
    
    - removed create_site, creating site on start was keeping Jekyll from
      picking up changes in the configuration files.
    
    - Removed run_on_additions, run_on_modifications, run_on_removals
      because run_on_changes is called in all these cases.
    
    - renamed jekyll! to build, jekyll! was a really non-descriptive name,
      also site is created in-place here using the current options.

--- lib/guard/jekyll.rb
+++ lib/guard/jekyll.rb
@@ -5,62 +5,43 @@ require 'jekyll'
 
 module Guard
   class Jekyll < Guard
-
     def initialize(watchers = [], options = {})
-      super
-      @source = options[:source] || File.dirname('.')
+      defaults = {
+        :source => './',
+        :destination => './_site',
+        :config => ['_config.yml'],
+      }
+
+      super(watchers, options.merge(defaults))
     end
 
     def start
       UI.info 'Guard::Jekyll is watching for file changes'
-      create_site(@source)
+      rebuild
     end
 
     def run_all
-      jekyll!
+      rebuild
     end
 
     def run_on_changes(paths)
-      jekyll!
-    end
-
-    def run_on_additions(paths)
-      jekyll!
-    end
-
-    def run_on_modifications(paths)
-      jekyll!
-    end
-
-    def run_on_removals(paths)
-      jekyll!
+      rebuild
     end
 
     private
 
-    def jekyll!
-      UI.info 'Guard::Jekyll running'
+    # rebuilds the entire jekyll site
+    #
+    def rebuild
+      UI.info 'Guard::Jekyll regenerating'
 
-      @jekyll_site.process
+      site = ::Jekyll::Site.new(::Jekyll.configuration(options))
+      site.process
 
-      UI.info 'Guard::Jekyll complete'
+      UI.info 'Guard::Jekyll done.'
     rescue Exception => e
       UI.error "Guard::Jekyll failed: #{e}"
       throw :task_has_failed
-    end
-
-    def create_site(source)
-      working_path = File.expand_path(source)
-
-      options = {'source' => working_path}
-
-      unless File.exists? File.join(working_path, '_config.yml')
-        options['destination'] = File.join(working_path, '_site')
-        options['plugins'] = File.join(working_path, '_plugins')
-      end
-
-      config = ::Jekyll.configuration(options)
-      @jekyll_site = ::Jekyll::Site.new(config)
     end
   end
 end
